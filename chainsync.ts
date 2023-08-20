9415228efc9fc09c76736decf042852a5ae2f60b
Ian Shehadeh <ian@shehadeh.net>
2023-08-20 07:29:59 -0400

    better syncing, remove chain for dest

--- chainsync.ts
+++ chainsync.ts
@@ -1,9 +1,8 @@
 import { PageModifiedEvent } from "$sb/app_event.ts";
 import { editor, markdown, space } from "$sb/silverbullet-syscall/mod.ts";
-import {  ParseTree, findNodeOfType, traverseTree, nodeAtPos, traverseTreeAsync} from "$sb/lib/tree.ts";
+import {  ParseTree, findNodeOfType, replaceNodesMatching, renderToText} from "$sb/lib/tree.ts";
 import { Range, rangesOverlap } from "$sb/lib/change.ts";
 import { resolvePath } from "$sb/lib/resolve.ts";
-import { events, mq } from "$sb/plugos-syscall/mod.ts";
 
 export function findNodeTypeInRange(tree: ParseTree, type: string, range: Range): ParseTree | null {
     if (tree.from === undefined || tree.to === undefined) {
@@ -27,48 +26,64 @@ export function findNodeTypeInRange(tree: ParseTree, type: string, range: Range)
 
     return null
 }
+
+export type ChainSyncLinkInfo = {
+    page: string;
+    pos: number;
+}
+
 export async function onPageModified(ev: PageModifiedEvent): Promise<void> {
     const pageName = await editor.getCurrentPage();
     const doc = await editor.getText();
     const pageTree = await markdown.parseMarkdown(doc);
-    for (const change of ev.changes) {
+    for await(const change of ev.changes) {
         const taskAtPos = findNodeTypeInRange(pageTree, "Task", change.oldRange);
         if (taskAtPos) {
             console.log("found task ", taskAtPos)
-            traverseTreeAsync(taskAtPos, async (node) => {
-                console.log(node)
+            let link: ChainSyncLinkInfo | undefined;
+
+            // look for any chain icon wiki links in the tree, remove them and copy out their info.
+            replaceNodesMatching(taskAtPos, (node) => {
                 if (node.type === "WikiLink") {
                     console.log("found wiki link ", node)
                     const wikiLinkAlias = findNodeOfType(node, "WikiLinkAlias");
-                    console.log("alias text  ", wikiLinkAlias?.children![0].text! )
                     if (wikiLinkAlias?.children![0].text! === "🔗") {
                         const wikiLinkPage = findNodeOfType(node, "WikiLinkPage")!;
                         console.log("found chain link page = ", wikiLinkPage);
                         try {
                             const [targetPageName, targetLoc] = resolvePath(pageName, wikiLinkPage.children![0].text!).split("@");
-                            console.log(`chain link to "${targetPageName}@${targetLoc}" from "${pageName}"`);
-                            if (targetPageName == pageName) {
-                                console.log("same page, skipping")
-                                return true;
-                            }
-                            const targetDoc = await space.readPage(targetPageName);
-                            const targetParseTree = await markdown.parseMarkdown(targetDoc);
-                            const targetNode = nodeAtPos(targetParseTree, Number.parseInt(targetLoc, 10));
-                            if (targetNode) {
-                                const srcTaskText = doc.slice(taskAtPos.from, taskAtPos.to);
-                                const newTargetDoc = targetDoc.slice(0, targetNode.from) + srcTaskText + targetDoc.slice(taskAtPos.from);
-                                await space.writePage(targetPageName, newTargetDoc);
-                            }
+                            
+                            link = { page: targetPageName, pos: Number.parseInt(targetLoc, 10)}
+                            return null;
                         } catch(e) {
                             console.log(e);
                         }
                     }
-
-                    return true;
                 }
 
-                return false;
-            })
+                return node;
+            });
+
+            if (link) {
+                console.log(`chain link to "${link.page}@${link.pos}" from "${pageName}"`);
+
+                if (link.page == pageName) {
+                    console.log("same page, skipping")
+                    continue
+                }
+                const targetDoc = await space.readPage(link.page);
+                const targetParseTree = await markdown.parseMarkdown(targetDoc);
+                const targetNode = findNodeTypeInRange(targetParseTree, "Task", {from: link.pos, to: link.pos + 1});
+                if (targetNode) {
+                    const srcTaskText = renderToText(taskAtPos);
+                    const destTaskText = targetDoc.slice(targetNode.from, targetNode.to);
+                    if (srcTaskText !== destTaskText) {
+                        console.log(`src and dest do not match: "${srcTaskText}" !== "${destTaskText}"`)
+                        const newTargetDoc = targetDoc.slice(0, targetNode.from) + srcTaskText + targetDoc.slice(targetNode.to);
+                        await space.writePage(link.page, newTargetDoc);
+                    }
+                }
+            }
             console.log(taskAtPos)
         }
     }
