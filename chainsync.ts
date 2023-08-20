10312847c206709ab9dbc070dc0120c06f942e38
Ian Shehadeh <ian@shehadeh.net>
2023-08-19 20:17:11 -0400

    first attempt at syncing tasks

--- chainsync.ts
+++ chainsync.ts
@@ -1,7 +1,9 @@
 import { PageModifiedEvent } from "$sb/app_event.ts";
-import { editor, markdown } from "$sb/silverbullet-syscall/mod.ts";
-import {  ParseTree} from "$sb/lib/tree.ts";
+import { editor, markdown, space } from "$sb/silverbullet-syscall/mod.ts";
+import {  ParseTree, findNodeOfType, traverseTree, nodeAtPos, traverseTreeAsync} from "$sb/lib/tree.ts";
 import { Range, rangesOverlap } from "$sb/lib/change.ts";
+import { resolvePath } from "$sb/lib/resolve.ts";
+import { events, mq } from "$sb/plugos-syscall/mod.ts";
 
 export function findNodeTypeInRange(tree: ParseTree, type: string, range: Range): ParseTree | null {
     if (tree.from === undefined || tree.to === undefined) {
@@ -26,11 +28,45 @@ export function findNodeTypeInRange(tree: ParseTree, type: string, range: Range)
     return null
 }
 export async function onPageModified(ev: PageModifiedEvent): Promise<void> {
+    const page = await editor.getCurrentPage();
     const doc = await editor.getText();
     const pageTree = await markdown.parseMarkdown(doc);
-    for(const change of ev.changes) {
+    for (const change of ev.changes) {
         const taskAtPos = findNodeTypeInRange(pageTree, "Task", change.oldRange);
         if (taskAtPos) {
+            console.log("found task ", taskAtPos)
+            traverseTreeAsync(taskAtPos, async (node) => {
+                console.log(node)
+                if (node.type === "WikiLink") {
+                    console.log("found wiki link ", node)
+                    const wikiLinkAlias = findNodeOfType(node, "WikiLinkAlias");
+                    console.log("alias text  ", wikiLinkAlias?.children![0].text! )
+                    if (wikiLinkAlias?.children![0].text! === "🔗") {
+                        const wikiLinkPage = findNodeOfType(node, "WikiLinkPage")!;
+                        console.log("found chain link page = ", wikiLinkPage);
+                        try {
+                            const [targetPage, targetLoc] = resolvePath(page, wikiLinkPage.children![0].text!).split("@");
+                            const targetDoc = await space.readPage(targetPage);
+                            if (targetDoc == page) {
+                                return true;
+                            }
+                            const targetParseTree = await markdown.parseMarkdown(targetDoc);
+                            const targetNode = nodeAtPos(targetParseTree, Number.parseInt(targetLoc, 10));
+                            if (targetNode) {
+                                const srcTaskText = doc.slice(taskAtPos.from, taskAtPos.to);
+                                const newTargetDoc = targetDoc.slice(0, targetNode.from) + srcTaskText + targetDoc.slice(taskAtPos.from);
+                                await space.writePage(targetDoc, newTargetDoc);
+                            }
+                        } catch(e) {
+                            console.log(e);
+                        }
+                    }
+
+                    return true;
+                }
+
+                return false;
+            })
             console.log(taskAtPos)
         }
     }
